0b1cb86d0ee9e3cd5c4d30119a7a3ebe4577d413
nikki.zand <nzand54@gmail.com>
2019-05-13 20:49:00 -0400

    commit 1

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN NOT NULL,
+	PRIMARY KEY (id)
+);
+
+use burgers_db;
+
+select  * from burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES 
+("Veggie Burger", False);
+
+INSERT INTO burgers (burger_name, devoured) VALUES 
+("Double Secret Sauce Patty", False);
+
+INSERT INTO burgers (burger_name, devoured) VALUES 
+("Chicken Ranch BBQ", False);
+
+INSERT INTO burgers (burger_name, devoured) VALUES 
+("San Jose Fire Burger", False);
+
+select  * from burgers;

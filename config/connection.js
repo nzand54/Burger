8d7e9cecf146ad864e07e31478d0c3fb54f39a74
nikki.zand <nzand54@gmail.com>
2019-05-13 21:52:22 -0400

    heroku config

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Default",
-  database: "burgers_db"
-});
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection ({
+        host: 'jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'chfevca6bkk386r9',
+        password:'xsyl79vbuhvns117',
+        database: 'q829ow3lxjezethc'
+    });
+  };
+
+
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Default",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {

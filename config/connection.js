ad5a114194bf3dd2ea6afa978039a125da5573b5
nikki.zand <nzand54@gmail.com>
2019-05-13 21:55:59 -0400

    heroku config

--- config/connection.js
+++ config/connection.js
@@ -14,15 +14,6 @@ if (process.env.JAWSDB_URL) {
   };
 
 
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "Default",
-//   database: "burgers_db"
-// });
-
 // Make connection.
 connection.connect(function(err) {
   if (err) {
